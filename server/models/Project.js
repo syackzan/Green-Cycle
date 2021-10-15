cb2b0c17c050e2c680d4e907315f5cf17502bf86
Scotty Yackzan <syackzan@gmail.com>
2021-10-15 16:37:10 -0700

    Added Project Schema/Model

---
+++ server/models/Project.js
@@ -0,0 +1,51 @@
+const { Schema, model } = require('mongoose');
+
+
+const projectSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    address: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    city: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    state: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    zip: {
+        type: Number,
+        required: true,
+        trim: true
+    },
+    owner: {
+        type: String,
+        required: false,
+        trim: true
+    },
+    generalContractor: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'generalContractor'
+        }
+    ],
+    recycleItems: [
+        {
+            type:Schema.Types.ObjectId,
+            ref: "Item"
+        }
+    ]
+})
+
+const Project = model('Project', projectSchema);
+
+module.exports = Project;
