6d4d95ddf3f72345bd54c2306b3dc8fdb399e243
Scotty Yackzan <syackzan@gmail.com>
2021-10-16 15:47:12 -0700

    graphql wasn't working, so updated package.json to old apollo-server-express 2.12 where Error for 'requiring await server.start()' was being thrown

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/greenCycle-db',
-  {
-  
-  }
-);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/greenCycle-db');
 
 module.exports = mongoose.connection;
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,25 @@
+const { GeneralContractor, Project, Item } = require('../models');
 
 const resolvers = {
+    Query: {
+        contractors: async () => {
+            // Populate all GC's, there projects, and items
+            return await GeneralContractor.find({}).populate('projects').populate({
+                path: 'projects',
+                populate: 'recycleItems'
+            })
+        },
 
+        projects: async () => {
+            // Populate all Projects
+            return await Project.find({}).populate('recycleItems');
+        },
+
+        items: async () => {
+            // Populate all Recycled Items
+            return await Item.find({}).populate('projects');
+        }
+    }
 }
 
 module.exports = resolvers;
