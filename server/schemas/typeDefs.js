f0736c126283b5049765d91e8dd2aae9f669630d
Scotty Yackzan <syackzan@gmail.com>
2021-10-16 14:45:54 -0700

    Added get all type Defs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,49 @@
 const { gql } = require('apollo-server-express');
 
-const typeDefs = gql``;
+const typeDefs = gql`
+    type GeneralContractor {
+        _id: ID
+        companyName: String
+        address: String
+        city: String
+        state: String
+        zip: Int
+        phoneNumber: String
+        email: String
+        password: String
+        projects: [Project]
+    }
+
+    type Project {
+        _id: ID
+        name: String
+        type: String
+        squareFootage: Int
+        address: String
+        city: String
+        state: String
+        zip: Int
+        owner: String
+        generalContractor: GeneralContractor
+        recycleItems: [Item]
+    }
+
+    type Item {
+        _id: ID
+        material: String
+        date: String
+        quantity: Int
+        unit: String
+        notes: String
+        recycler: String
+        projects: Project
+    }
+
+    type Query {
+        contractors: [GeneralContractor]
+        projects: [Project]
+        items: [Item]
+    }
+`;
 
 module.exports = typeDefs;
